e544f4a0925e49e1227afbb167b58cda0c74f3d5
Arafat-erasung <arafaterasung@gmail.com>
2022-04-28 03:40:06 +0200

    added functionality to buttons

--- app.js
+++ app.js
@@ -12,31 +12,83 @@ class Calculator {
         this.operation = undefined
     }
 
-    deleter = () => {
-
+    delete = () => {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
     appendNumber = number => {
+        if(number === "." && this.currentOperand.includes('.')) return
         this.currentOperand = this.currentOperand.toString() + number.toString()
     }
 
     selectOperation = operation => {
-
+        if(this.currentOperand === "") return
+        if(this.prevOperand !== "") {
+            this.calculate()
+        }
+        this.operation = operation
+        this.prevOperand = this.currentOperand
+        this.currentOperand = ''
     }
 
     calculate = () => {
+        let result
+        const previous = parseFloat(this.prevOperand)
+        const current = parseFloat(this.currentOperand)
+        if(isNaN(previous) || isNaN(current)) return
+            switch(this.operation){
+                case "+":
+                    result = previous + current
+                break
+                case "-":
+                    result = previous - current
+                break
+                case "x":
+                    result = previous * current
+                break
+                case "÷":
+                    result = previous / current
+                break
+                default:
+                    return
+            }
+            this.currentOperand = result
+            this.operation = undefined
+            this.prevOperand = ''
 
+    }
+
+    getCommaAndDecimalNumber = number => {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+
+        let integerDisplay
+        if(isNaN(integerDigits)){
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {
+                maximumFractionDigits: 0
+            })
+        }
+
+        if (decimalDigits != null){
+            return `${integerDisplay}.${decimalDigits}`
+        }else {
+            return integerDisplay
+        }
     }
 
 
     updateDisplay = () => {
-        this.currentOperandInnerText.innerText = this.currentOperand
+        this.currentOperandInnerText.innerText = this.getCommaAndDecimalNumber(this.currentOperand)
+        if(this.prevOperand != null){
+            this.prevOperandInnerText.innerText = `${this.getCommaAndDecimalNumber(this.prevOperand)} ${this.operation}`
+        }else {
+            this.prevOperandInnerText = ''
+        }
     }
 }
-
-
-
-
 
 
 const numberButtons = document.querySelectorAll('[data-number]')
@@ -55,4 +107,26 @@ numberButtons.forEach(button => {
         calculator.appendNumber(button.innerText)
         calculator.updateDisplay()
     })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.selectOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.calculate()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clearAll()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
 })
